ec3cc2c427adec42fb9fc918436c333b32d80735
Yunxuan Yu <yunxuany@andrew.cmu.edu>
2021-05-17 21:21:10 +0800

    add antd css into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import '../node_modules/antd/dist/antd.css'
 import reportWebVitals from './reportWebVitals';
 import HorizontalLoginForm from './HorizontalLoginForm';
 
