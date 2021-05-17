0c08283f480f8ac6942859de12093bf86978971f
Yunxuan Yu <yunxuany@andrew.cmu.edu>
2021-05-17 18:06:53 +0800

    change index.js & add a new js

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HorizontalLoginForm from './HorizontalLoginForm';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HorizontalLoginForm />
   </React.StrictMode>,
   document.getElementById('root')
 );
